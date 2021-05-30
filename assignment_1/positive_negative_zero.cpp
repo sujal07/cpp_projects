b2b12e107c68345bfedd7eb506ea22eb0f1f15a4
Sujal Maharjan <sujalmhrzn07@gmail.com>
2021-05-30 08:41:55 +0545

    positive/negative/zero

---
+++ assignment_1/positive_negative_zero.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int number;
+    cout<<"Enter a number \n";
+    cin>>number;
+    if(number==0)
+    cout<<"The number is zero.";
+    else if(number>0)
+    cout<<"The number is positive.";
+    else
+    cout<<"The number is negative";
+
+}
