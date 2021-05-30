17c7a42a4e5ac13b8e6210786dad2fdb50f97ba6
Sujal Maharjan <sujalmhrzn07@gmail.com>
2021-05-30 08:35:41 +0545

    positive or negative

---
+++ assignment_1/positive_or_negative.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int number;
+    cout<<"Enter a number";
+    cin>>number;
+    if(number>0)
+    cout<<"The number is positive.";
+    else
+    cout<<"The number is negative.";
+
+}
