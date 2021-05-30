daf8fb7e73310fc881e2e5cc1ff09c0f40d551fd
Sujal Maharjan <sujalmhrzn07@gmail.com>
2021-05-30 08:46:08 +0545

    odd_even_zero

---
+++ assignment_1/odd_even_zero.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter a number \n";
+    cin>>n;
+    if(n==0)
+    cout<<"The number is 0";
+    else if(n%2==0)
+    cout<<"The number is even";
+    else 
+    cout<<"The number is odd";
+
+}
