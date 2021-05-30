f91fa1b213cfa5502146f5f5f159a14d57261f2f
Sujal Maharjan <sujalmhrzn07@gmail.com>
2021-05-30 09:20:40 +0545

    sum of n natural number

---
+++ assignment_1/sum_of_n_number.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,sum,i;
+    cout<<"Enter N which sum of natural numbers is to be found.";
+    cin>>n;
+    sum=0;
+    for(i=1;i<=n;i++){
+        sum=sum+i;
+
+    }
+    cout<<"The sum of n natural numbers is "<<sum;
+}
